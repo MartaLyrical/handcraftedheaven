b6cfa011326bf3a0e715da74c062d6e3144f587c
Thompson Nkomo <nko20005@byui.edu>
2024-06-04 20:37:48 +0200

    initial commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,10 +11,7 @@ const About = () => {
           <div className="details">
             <h1>About Us</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
-              soluta nesciunt, fuga veniam ducimus quos dolor velit ipsa numquam
-              ut culpa perferendis mollitia illo tempore, consectetur ex
-              recusandae expedita voluptatem.
+            Handcrafted Haven is a website that showcases the artwork of different artisans. The website allows potential customers to sign in, shop on the portal, and contribute to the fine work of all the artisans on the website. We believe in the philosophy that simplicity is the greatest sophistication. At Handcrafted Haven, we offer simple but high-quality products ranging from all types of jewelry that will catch your eye. Our jewelry ranges from diamond-coated, silver-coated, to gold-coated. Shop for whatever you desire in the comfort of your own home and get the best quality jewelry at an affordable price. We are committed to providing an unforgettable experience. Explore all that we have to offer and remember to sign up to register.
             </p>
           </div>
         </div>
@@ -24,10 +21,7 @@ const About = () => {
             <div className="left-col">
               <h1>Our Mission</h1>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
-                soluta nesciunt, fuga veniam ducimus quos dolor velit ipsa
-                numquam ut culpa perferendis mollitia illo tempore, consectetur
-                ex recusandae expedita voluptatem.
+              We strive to become the premier artisan store on earth. By offering the highest quality service to all our customers, we bring together people of all ages. Our goal is to cater to the needs of every individual, from the least affluent to the wealthiest, and provide unparalleled support regardless of their social status.
               </p>
             </div>
           </div>
